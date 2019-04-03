174c02151695bb7aa872252cf7771eb12171ec4c
Sam Pettersson <sam@sampettersson.com>
2019-04-03 16:15:34 +0200

    Fix swift url

--- bin/before_install.sh
+++ bin/before_install.sh
@@ -1,7 +1,7 @@
 DIR="$(pwd)"
 cd ..
 export SWIFT_VERSION=swift-4.2.1-RELEASE
-wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
+wget https://swift.org/builds/${SWIFT_VERSION}/ubuntu1404/${SWIFT_VERSION}-ubuntu14.04.tar.gz
 tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
 export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:${PATH}"
 cd "$DIR"
