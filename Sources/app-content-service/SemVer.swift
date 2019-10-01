6ed241767fccb3efc950a2e842c494ed1a8cbb02
Sam Pettersson <sam@sampettersson.com>
2019-10-01 13:38:47 +0200

    Fix SemVer crash

--- Sources/app-content-service/SemVer.swift
+++ Sources/app-content-service/SemVer.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+extension Collection {
+
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
 struct SemVer {
     let major: Int
     let minor: Int
@@ -15,10 +23,16 @@ struct SemVer {
     static func parse(version: String) -> SemVer {
         let parts = version.split(separator: ".")
         
-        guard let major = Int(String(parts[0])),
-            let minor = Int(String(parts[1])),
-            let patch = Int(String(parts[2])) else {
-                return SemVer(major: 0, minor: 0, patch: 0)
+        let fallbackSemVer = SemVer(major: 0, minor: 0, patch: 0)
+        
+        let partOne = parts[safe: 0] ?? "0"
+        let partTwo = parts[safe: 1] ?? "0"
+        let partThree = parts[safe: 2] ?? "0"
+        
+        guard let major = Int(String(partOne)),
+            let minor = Int(String(partTwo)),
+            let patch = Int(String(partThree)) else {
+                return fallbackSemVer
         }
         
         return SemVer(
